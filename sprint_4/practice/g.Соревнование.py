8f712598f76fa249111e2bbf37e8832d53870338
Ковылин Василий <coherentus@gmail.com>
2022-03-04 17:09:43 +0300

    sp_4 work at g

--- sprint_4/practice/g.Соревнование.py
+++ sprint_4/practice/g.Соревнование.py
@@ -1,35 +1,34 @@
-def find_long_range(arr, start):
-    """Вернуть длину последовательности.
-
-    """
-    result = 0
-    idx = start
-    parity = 0  # zero - is parity, not zero - not parity
-    while idx < len(arr) - 1:  # out of range guard
-        parity = parity + 1 if arr[idx] else parity - 1
-        parity = parity + 1 if arr[idx + 1] else parity - 1
-        idx += 2
-        if not parity:
-            result += idx - start
-    return result
-
-
 def find_max_range(arr):
-    max_len = 0
-    for i in range(len(arr)):
-        loc_start = i
-        if max_len < len(arr) - i:
-            cur_len = find_long_range(arr, loc_start)
-            if cur_len > max_len:
-                max_len = cur_len
-            loc_start += cur_len
+    pref_arr = [None] * len(arr)
+    pref_arr[0] = arr[0]
+    for i in range(1, len(arr)):
+        pref_arr[i] = pref_arr[i - 1] + arr[i]
 
-    return max_len
+    first_val_idx = dict()
+    last_val_idx = dict()
+
+    for i in range(len(arr)):
+        key = pref_arr[i]
+        last_val_idx[key] = i
+        if pref_arr[i] not in first_val_idx:
+            first_val_idx[key] = i
+
+    max_size = 0
+    for key in first_val_idx:
+        size = last_val_idx[key] - first_val_idx[key]
+        if size > max_size:
+            max_size = size
+    return max_size
 
 
 def main():
-    if int(input()):
-        rounds = list(map(int, str(input()).split()))
+    size = int(input())
+    if size:
+        rounds = [None] * size
+        rounds = input().split()
+        for i in range(size):
+            rounds[i] = -1 if rounds[i] == '0' else 1
+        # list(map(int, str(input()).split()))
         print(find_max_range(rounds))
     else:
         print('0')
