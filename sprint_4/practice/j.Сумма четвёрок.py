12390e35eebbec3d3206929b319b062216a2ecf3
coherentus <75427478+coherentus@users.noreply.github.com>
2022-03-07 01:03:51 +0300

    sp_4 aad j

---
+++ sprint_4/practice/j.Сумма четвёрок.py
@@ -0,0 +1,12 @@
+def find_sums():
+    pass
+
+
+def main():
+    count_nums = int(input())
+    second_word = input()
+    print(find_sums(count_nums, second_word))
+
+
+if __name__ == '__main__':
+    main()
