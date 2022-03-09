c6eaed04dcb4a8a34b713a9c89198ed17b5bcbbe
Ковылин Василий <coherentus@gmail.com>
2022-03-09 18:22:28 +0300

    sp_4 final A

--- sprint_4/final/a.search_system.py
+++ sprint_4/final/a.search_system.py
@@ -1,30 +1,40 @@
-def get_relevant(request, documents):
+def get_relevant(request, words_in_docs):
     answer = list()
-    req_words = set(request.split())
+    words_stat = dict()
     for word in request:
-
-    
+        if word in words_in_docs:
+            for doc_num, count_in_doc in words_in_docs[word].items():
+                if doc_num in words_stat:
+                    words_stat[doc_num] += count_in_doc
+                else:
+                    words_stat[doc_num] = count_in_doc
+    for key, value in words_stat.items():
+        answer.append((value, key))
+    answer = sorted(answer, reverse=True)[0:5]
+    answer = [x[1] for x in answer]
+    # print(answer[0:5])
+        # {num_doc: count}
+    return answer
 
 
 def main():
     num_documents = int(input())
-    documents_db = list()
-    for count in range(num_documents):
-        document_in = input().split
-        document = dict()
+    # db format key - word, values - {number_of_doc: count_in_doc}
+    words_db = dict()
+    for num_doc in range(1, num_documents + 1):
+        document_in = input().split()
         for word in document_in:
-            try:
-                document[word] += 1
-            except IndexError:
-                document[word] = 1
-        # порядковый номер
-        documents_db[count][0] = count + 1
-        # список слов
-        documents_db[count][1] = document
-    
+            if word in words_db:
+                if num_doc in words_db[word]:
+                    words_db[word][num_doc] += 1
+                else:
+                    words_db[word][num_doc] = 1
+            else:
+                words_db[word] = {num_doc: 1}
+
     num_requests = int(input())
-    for count in num_requests:
-        result = get_relevant(input(), documents_db)
+    for req_count in range(num_requests):
+        result = get_relevant(input().split(), words_db)
         print(*result)
 
 
