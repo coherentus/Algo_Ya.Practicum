5d397f1d92fc6777fba8192a35aed1400cdc4423
Ковылин Василий <coherentus@gmail.com>
2022-03-14 11:48:48 +0300

    sp_4 practice j OK

--- sprint_4/practice/j.Сумма четвёрок.py
+++ sprint_4/practice/j.Сумма четвёрок.py
@@ -1,42 +1,42 @@
-def find_sums(arr, nums, need_sum):
+def find_sums(arr,  need_sum):
     result = list()
-    res_str = dict()
-    for pos_1 in range(0, len(arr) - 3):
-        str_1 = arr[pos_1]
-        sum_1 = nums[pos_1]
-        for pos_2 in range(pos_1 + 1, len(arr) - 2):
-            str_2 = arr[pos_2]
-            sum_2 = sum_1 + nums[pos_2]
-            for pos_3 in range(pos_2 + 1, len(arr) - 1):
-                str_3 = arr[pos_3]
-                sum_3 = sum_2 + nums[pos_3]
-                for pos_4 in range(pos_3 + 1, len(arr)):
-                    str_4 = arr[pos_3]
-                    sum_4 = sum_3 + nums[pos_4]
-                    if sum_4 == need_sum:
-                        cur_candid = sorted([
-                            arr[pos_1],
-                            arr[pos_2],
-                            arr[pos_3],
-                            arr[pos_4],
-                        ])
-                        res_str[tuple(sorted([str_1, str_2, str_3, str_4]))] = cur_candid
-                        
-    return sorted(res_str.values())
+    sums2_dict = {}
+    # суммы двоек
+    for pos_1 in range(0, len(arr) - 1):
+        for pos_2 in range(pos_1 + 1, len(arr)):
+            sum2 = arr[pos_1] + arr[pos_2]
+            if sum2 in sums2_dict:
+                tmp = sums2_dict[sum2]
+                tmp.append((pos_1, pos_2))
+                sums2_dict[sum2] = tmp
+            else:
+                sums2_dict[sum2] = [(pos_1, pos_2)]
+
+    for key in sums2_dict:
+        degree_4 = need_sum - key
+        if degree_4 in sums2_dict:
+            first_2 = sums2_dict[key]
+            second_2 = sums2_dict[degree_4]
+            for pos_1 in first_2:
+                for pos_2 in second_2:
+                    candid = list(pos_1) + list(pos_2)
+                    if len(set(candid)) == len(candid):
+                        res_nums = [arr[x] for x in candid]
+                        result.append(tuple(sorted(res_nums)))
+
+    return sorted(set(result))
 
 
 def main():
     count_nums = int(input())
     needed_sum = int(input())
     if count_nums:
-        nums_arr = [None] * count_nums
         input_arr = [None] * count_nums
         idx = 0
         for in_num in input().split():
-            input_arr[idx] = in_num
-            nums_arr[idx] = int(in_num)
+            input_arr[idx] = int(in_num)
             idx += 1
-        answer = find_sums(input_arr, nums_arr, needed_sum)
+        answer = find_sums(input_arr, needed_sum)
         print(len(answer))
         for answ in answer:
             print(*answ)
