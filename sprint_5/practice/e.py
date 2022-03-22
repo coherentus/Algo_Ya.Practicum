0546c5be78c0e72f2982c01728557f528a899239
coherentus <75427478+coherentus@users.noreply.github.com>
2022-03-23 00:38:03 +0300

    sp_5_p edit e

--- sprint_5/practice/e.py
+++ sprint_5/practice/e.py
@@ -17,11 +17,26 @@ def solution(root):
     #  “ヽ(´▽｀)ノ”
     # nodes_values = list()
     def step(node):
-        if not (
-            (node.left and node.left.value < node.value)
-            and (node.right and node.right.value > node.value)
-        )
+        left_ok, right_ok = False, False
+        if node.left:
+            left_ok = node.left.value < node.value
+        else:
+            left_ok = True
+            print('l ', left_ok)
+        if node.right:
+            right_ok = node.value < node.right.value
+        else:
+            right_ok = True
+            print('r ', right_ok)
+        print('l and r ', not (left_ok and right_ok))
+        if not left_ok or not right_ok:
             return False
+
+        """if not (
+            (node.left and (node.left.value < node.value))
+            and (node.right and (node.right.value > node.value))
+        ):
+            return False"""
         if node.left:
             step(node.left)
         if node.right:
