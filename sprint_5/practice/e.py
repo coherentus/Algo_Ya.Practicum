c352d979a242586f045fa6a95e303be86fee9352
Ковылин Василий <coherentus@gmail.com>
2022-03-23 11:44:54 +0300

    sp_5_p  i e OK

--- sprint_5/practice/e.py
+++ sprint_5/practice/e.py
@@ -17,29 +17,23 @@ def solution(root):
     #  “ヽ(´▽｀)ノ”
     # nodes_values = list()
     def step(node):
-        left_ok, right_ok = False, False
-        if node.left:
-            left_ok = node.left.value < node.value
-        else:
-            left_ok = True
-            print('l ', left_ok)
-        if node.right:
-            right_ok = node.value < node.right.value
-        else:
-            right_ok = True
-            print('r ', right_ok)
-        print('l and r ', not (left_ok and right_ok))
-        if not left_ok or not right_ok:
+        if (node.left is None) and (node.right is None):
+            return
+        left_ok, right_ok = True, True
+        if node.left and (node.left.value >= node.value):
+            left_ok = False
+        if node.right and (node.right.value <= node.value):
+            right_ok = False
+        
+        if (left_ok == False) or (right_ok == False):
             return False
-
-        """if not (
-            (node.left and (node.left.value < node.value))
-            and (node.right and (node.right.value > node.value))
-        ):
-            return False"""
+        
         if node.left:
-            step(node.left)
+            if step(node.left) == False:
+                return False
         if node.right:
-            step(node.right)
-    step(root)
+            if step(node.right) == False:
+                return False
+    if step(root) == False:
+        return False
     return True
---
+++ sprint_5/practice/i.py
@@ -0,0 +1,16 @@
+def factorial(n):
+    f = 1
+    for i in range (1, n + 1):
+        f = f * i
+    return f
+
+def answ(num):  # числа каталана
+    return int(factorial(2 * num) / (factorial(num) * factorial(num + 1)))
+
+def main():
+    n = int(input())    
+    print(answ(n))
+
+
+if __name__ == '__main__':
+    main()
