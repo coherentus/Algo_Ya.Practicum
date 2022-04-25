cf969f87883559a70c550b2748099951a2146939
Ковылин Василий <coherentus@gmail.com>
2022-04-25 13:34:35 +0300

    add m

--- sprint_5/practice/code.py
+++ sprint_5/practice/code.py
@@ -1,20 +1,8 @@
-# Comment it before submitting
-# class Node:  
-#     def __init__(self, value, left=None, right=None):  
-#         self.value = value  
-#         self.right = right  
-#         self.left = left
-
-
-def solution(root):
+def sift_up(heap, idx):
     #  Your code
     #  “ヽ(´▽｀)ノ”
     pass
-
-
+            
 def test():
-    node1 = Node(1)
-    node2 = Node(-5)
-    node3 = Node(3, node1, node2)
-    node4 = Node(2, node3, None)
-    assert solution(node4) == 3
+    sample = [-1, 12, 6, 8, 3, 15, 7]
+    assert sift_up(sample, 5) == 1
