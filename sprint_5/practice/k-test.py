ec8018cfd19eb62ed078e182beaae51d6093399e
coherentus <75427478+coherentus@users.noreply.github.com>
2022-05-14 08:49:14 +0300

    sp6 add all template

--- sprint_5/practice/k-test.py
+++ sprint_5/practice/k-test.py
@@ -1,36 +1,29 @@
-# do not declare Node in your submit-file 
-class Node: 
-    def __init__(self, left=None, right=None, value=0): 
-        self.value = value 
-        self.right = right 
-        self.left = left 
-################# 
+# do not declare Node in your submit-file
+class Node:
+    def __init__(self, left=None, right=None, value=0):
+        self.value = value
+        self.right = right
+        self.left = left
+#################
+
+
 def print_range(node: Node, l: int, r: int):
-    
+
     def print_forward(node, left, right):
         if node is None:
             return
 
         if left <= node.value <= right:
             print(node.value)
-    
-        if not node.right is None:
+
+        if node.right is not None:
             print_forward(node.right, left, right)
-        if not node.left is None:
+        if node.left is not None:
             print_forward(node.left, left, right)
-    
+
     print_forward(node, l, r)
 
-"""
-5
-1 7 2 4
-2 3 3 -1
-3 1 -1 -1
-4 11 -1 5
-5 15 -1 -1
-6
-14
-"""
+
 def main():
     node5 = Node(None, None, 15)
     node4 = Node(None, node5, 11)
@@ -42,6 +35,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
--- sprint_5/practice/m.py
+++ sprint_5/practice/m.py
@@ -1,17 +1,3 @@
-"""
-функция heap_add(heap, key):
-    index = heap.size + 1
-    heap[index] = key
-    sift_up(heap, index)
-
-функция sift_up(heap, index):
-    если index == 1, то
-        завершить работу
-    parent_index = index / 2  (целочисленное деление)
-    если heap[parent_index] < heap[index], то
-        обменять местами heap[parent_index] и heap[index]
-        sift_up(heap, parent_index)
-"""
 def sift_up(heap: list, idx: int) -> int:
     if idx == 1:
         return 1
@@ -19,11 +5,5 @@ def sift_up(heap: list, idx: int) -> int:
     if heap[parent_index] < heap[idx]:
         heap[parent_index], heap[idx] = heap[idx], heap[parent_index]
         idx = sift_up(heap, parent_index)
-    
-    return idx
 
-"""            
-def test():
-    sample = [-1, 12, 6, 8, 3, 15, 7]
-    assert sift_up(sample, 5) == 1
-"""
+    return idx
