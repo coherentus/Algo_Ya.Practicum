b5067fa8c2d50475984bdcb6c6b07df0c68877a6
coherentus <coherentus@gmail.com>
2022-05-17 17:26:50 +0300

    ыфьу цщклы

--- sprint_6/practice/C.DFS-test.py
+++ sprint_6/practice/C.DFS-test.py
@@ -1,28 +1,30 @@
-def dfs(graph, start, visited=None):
-    if visited is None:
+def dfs(graph, start, colors):  # visited=None
+    """if visited is None:
         visited = set()
-    visited.add(start)
-    print(start)
-    for next in graph[start] - visited:
-        dfs(graph, next, visited)
+    visited.add(start)"""
+    print(start, end=' ')
+    colors[start] = 'gray'
+    next_set = sorted(x for x in graph[start] if colors[x] == 'white')  # graph[start] - visited
+    for next in next_set:
+        dfs(graph, next, colors)
 
 def main():
     num_vert, num_edg = map(int, input().split())  # кол-во вершин и рёбер
-    vertexs = [[] for i in range(num_vert)]
+    vertexs = [set() for i in range(num_vert + 1)]
+    color = ['white' for i in range(num_vert + 1)]
     # считывание и обработка рёбер
     for _ in range(num_edg):
         # вершины, соединяемые ребром
         vert_1, vert_2 = map(int, input().split())
         # для вершины vert_1 (vertexs[vert_1 - 1]) обновить список вершин
-        if vert_2 not in vertexs[vert_1 - 1]:
-            vertexs[vert_1 - 1].append(vert_2)
-        """if vert_1 not in vertexs[vert_2 - 1]:
-            vertexs[vert_2 - 1].append(vert_1)"""
-    for count in range(num_vert):
-        vertexs[count] = sorted(vertexs[count])
+        vertexs[vert_1].add(vert_2)
+        vertexs[vert_2].add(vert_1)
     start_vertex = int(input())
 
-    dfs(vertexs, start_vertex)
+    if num_vert == 1:
+        print(start_vertex)
+    else:
+        dfs(vertexs, start_vertex, color)
 
     
 if __name__ == '__main__':
