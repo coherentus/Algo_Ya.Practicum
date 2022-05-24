a74df5bd5e54892dc1ae20658b01eab9f5665b57
Ковылин Василий <coherentus@gmail.com>
2022-05-24 11:02:26 +0300

    sp6 practice c.DFS OK

--- sprint_6/practice/C.DFS.py
+++ sprint_6/practice/C.DFS.py
@@ -1,29 +1,27 @@
 def dfs(vert_arr, num_vertex, color):  # v - номер вершины
     color[num_vertex] = 'gray'  # Вершина посещена, но ещё не обработана.
-    stack = []
     print(num_vertex, end=' ')
-    
-    stack.append(num_vertex)
+
+    stack = []
+    for num in vert_arr[num_vertex]:
+        if color[num] == 'white':
+            stack.append(num)
+
     while stack:
         fromm = stack.pop()
         if color[fromm] == 'white':
             print(fromm, end=' ')
             color[fromm] = 'gray'
-        for num in vert_arr[fromm]:
-            if color[num] == 'white':
-                stack.append(num)
-    
-    """# для каждого исходящего ребра (v,w): возьмём вершину w
-    for vertex in vert_arr[num_vertex]:
-        if color[vertex - 1] == 'white':  # Если вершина не посещена, то
-            dfs(vert_arr, vertex - 1, color)  # запустим обход от найденной смежной вершины.
-    color[num_vertex] = 'black'  # Теперь вершина обработана."""
-    
+            for num in vert_arr[fromm]:
+                if color[num] == 'white':
+                    stack.append(num)
+
 
 def main_dfs(vert_arr, start):
     # Длина массива равна числу вершин |V| + 1
     color = ['white' for i in range(len(vert_arr) + 1)]
     dfs(vert_arr, start, color)
+
 
 def main():
     num_vert, num_edg = map(int, input().split())  # кол-во вершин и рёбер
@@ -35,20 +33,17 @@ def main():
         # для вершины vert_1 (vertexs[vert_1 - 1]) обновить список вершин
         vertexs[vert_1].add(vert_2)
         vertexs[vert_2].add(vert_1)
-        """if vert_2 not in vertexs[vert_1]:
-            vertexs[vert_1].append(vert_2)
-        if vert_1 not in vertexs[vert_2]:
-            vertexs[vert_2].append(vert_1)"""
-    for count in range(num_vert):
+
+    for count in range(num_vert + 1):
         vertexs[count] = sorted(vertexs[count], reverse=True)
     start_vertex = int(input())
-    
+
     if num_vert == 1:
         print(start_vertex)
         return
 
     main_dfs(vertexs, start_vertex)
 
-    
+
 if __name__ == '__main__':
     main()
