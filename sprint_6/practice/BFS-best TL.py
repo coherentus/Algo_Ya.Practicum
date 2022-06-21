83d5894170d7c8b8c802af424e0f50f4e60bb1c2
Ковылин Василий <coherentus@gmail.com>
2022-06-21 15:16:01 +0300

    D. BFS edited

---
+++ sprint_6/practice/BFS-best TL.py
@@ -0,0 +1,49 @@
+def bfs(vert_arr, num_vertex, color):  # v - номер вершины
+    color[num_vertex] = 'gray'  # Вершина посещена, но ещё не обработана.
+    print(num_vertex, end=' ')
+
+    sequ = []
+    for num in vert_arr[num_vertex]:
+        if color[num] == 'white':
+            sequ.append(num)
+
+    while sequ:
+        fromm = sequ.pop(0)
+        if color[fromm] == 'white':
+            print(fromm, end=' ')
+            color[fromm] = 'gray'
+            for num in vert_arr[fromm]:
+                if color[num] == 'white':
+                    sequ.append(num)
+
+
+def main_bfs(vert_arr, start):
+    # Длина массива равна числу вершин |V| + 1
+    color = ['white' for i in range(len(vert_arr) + 1)]
+    bfs(vert_arr, start, color)
+
+
+def main():
+    num_vert, num_edg = map(int, input().split())  # кол-во вершин и рёбер
+    vertexs = [set() for i in range(num_vert + 1)]
+    # считывание и обработка рёбер
+    for _ in range(num_edg):
+        # вершины, соединяемые ребром
+        vert_1, vert_2 = map(int, input().split())
+        # для вершины vert_1 (vertexs[vert_1 - 1]) обновить список вершин
+        vertexs[vert_1].add(vert_2)
+        vertexs[vert_2].add(vert_1)
+
+    for count in range(num_vert + 1):
+        vertexs[count] = sorted(vertexs[count])
+    start_vertex = int(input())
+
+    if num_vert == 1:
+        print(start_vertex)
+        return
+
+    main_bfs(vertexs, start_vertex)
+
+
+if __name__ == '__main__':
+    main()
