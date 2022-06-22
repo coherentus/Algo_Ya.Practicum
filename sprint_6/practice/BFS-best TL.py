a0e1ff45ef85650a1b93abf2786009790a566a16
coherentus <75427478+coherentus@users.noreply.github.com>
2022-06-23 01:25:35 +0300

    edit

--- sprint_6/practice/BFS-best TL.py
+++ sprint_6/practice/BFS-best TL.py
@@ -1,38 +1,66 @@
-def bfs(vert_arr, num_vertex, color):  # v - номер вершины
-    color[num_vertex] = 'gray'  # Вершина посещена, но ещё не обработана.
-    print(num_vertex, end=' ')
+import sys
+from queue import Queue
 
-    sequ = []
+
+def main_bfs(vert_arr, num_vertex):  # num_vertex - номер вершины
+    result = [None] * len(vert_arr)
+    res_count = 0
+    color = ['white' for _ in range(len(vert_arr) + 1)]
+    
+    
+    """print(num_vertex, end=' ')"""
+    result[res_count] = num_vertex
+
+    """sequ = []
     for num in vert_arr[num_vertex]:
         if color[num] == 'white':
-            sequ.append(num)
+            sequ.append(num)"""
+    sequ = Queue(100000 + 1)
+    sequ.put(num_vertex)
+    color[num_vertex] = 'gray'  # Вершина посещена, но ещё не обработана.
 
-    while sequ:
+    while not sequ.empty():
+        fromm = sequ.get()
+        for num in vert_arr[fromm]:
+            if color[num] == 'white':
+                """print(num, end=' ')"""
+                res_count += 1
+                result[res_count] = fromm
+                color[num] = 'gray'
+                sequ.put(num)
+    
+    
+    
+    
+    """while sequ:
         fromm = sequ.pop(0)
         if color[fromm] == 'white':
-            print(fromm, end=' ')
+            
+            #print(fromm, end=' ')
+            res_count += 1
+            result[res_count] = fromm
+            
             color[fromm] = 'gray'
             for num in vert_arr[fromm]:
                 if color[num] == 'white':
-                    sequ.append(num)
+                    sequ.append(num)"""
 
-
-def main_bfs(vert_arr, start):
-    # Длина массива равна числу вершин |V| + 1
-    color = ['white' for i in range(len(vert_arr) + 1)]
-    bfs(vert_arr, start, color)
+    return result
 
 
 def main():
     num_vert, num_edg = map(int, input().split())  # кол-во вершин и рёбер
-    vertexs = [set() for i in range(num_vert + 1)]
+    vertexs = [set() for _ in range(num_vert + 1)]
     # считывание и обработка рёбер
     for _ in range(num_edg):
         # вершины, соединяемые ребром
-        vert_1, vert_2 = map(int, input().split())
+        """vert_1, vert_2 = map(int, input().split())"""
+        line = sys.stdin.readline().rstrip()
+        vert_1, vert_2 = line.split()
+        vert_1, vert_2 = int(vert_1), int(vert_2)
         # для вершины vert_1 (vertexs[vert_1 - 1]) обновить список вершин
         vertexs[vert_1].add(vert_2)
-        vertexs[vert_2].add(vert_1)
+        #vertexs[vert_2].add(vert_1)
 
     for count in range(num_vert + 1):
         vertexs[count] = sorted(vertexs[count])
@@ -42,7 +70,10 @@ def main():
         print(start_vertex)
         return
 
-    main_bfs(vertexs, start_vertex)
+    result = main_bfs(vertexs, start_vertex)
+    for res in result:
+        if res:
+            print(res, end=' ')
 
 
 if __name__ == '__main__':
