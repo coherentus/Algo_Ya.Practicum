cb31252dcf4a5308122125cfac0b9c5033e93eb7
Ковылин Василий <coherentus@gmail.com>
2022-06-29 12:07:22 +0300

    D.BFS OK

--- sprint_6/practice/D.BFS.py
+++ sprint_6/practice/D.BFS.py
@@ -1,53 +1,59 @@
 from queue import Queue
 
 
-def bfs(vert_arr, num_vertex, color):  # v - номер вершины
-    color[num_vertex] = 'gray'  # Вершина посещена, но ещё не обработана.
-    print(num_vertex, end=' ')
+def main_bfs(vert_arr, start_vert):
 
-    sequ = Queue(100000 + 1)
-    for num in vert_arr[num_vertex]:
-        if color[num] == 'white':
-            sequ.put(num)
-            print(num, end=' ')
-            color[num] = 'gray'
+    # Длины массивов равны числу вершин|V|.
+    color = ['white'] * len(vert_arr)
+    res_arr = list()
 
-    while not sequ.empty():
-        fromm = sequ.get()
-        if color[fromm] == 'white':
-            print(fromm, end=' ')
-            color[fromm] = 'gray'
-            for num in vert_arr[fromm]:
-                if color[num] == 'white':
-                    sequ.put(num)
+    # Создадим очередь вершин и положим туда стартовую вершину.
+    planned = Queue()
+    planned.put(start_vert)
+    color[start_vert] = 'gray'
+    
+    while not planned.empty():
+    
+        # Возьмём вершину из очереди.
+        cur_vert_num = planned.get()
+        cur_vert_arr = vert_arr[cur_vert_num]
+        res_arr.append(cur_vert_num)
+        for vertex_num in cur_vert_arr:
 
-
-def main_bfs(vert_arr, start):
-    # Длина массива равна числу вершин |V| + 1
-    color = ['white' for i in range(len(vert_arr))]
-    bfs(vert_arr, start, color)
-
+            if color[vertex_num] == 'white':
+                color[vertex_num] = 'gray'
+                planned.put(vertex_num) # Запланируем посещение вершины.
+    return res_arr
 
 def main():
-    num_vert, num_edg = map(int, input().split())  # кол-во вершин и рёбер
-    vertexs = [set() for i in range(num_vert + 1)]
-    # считывание и обработка рёбер
-    for _ in range(num_edg):
-        # вершины, соединяемые ребром
-        vert_1, vert_2 = map(int, input().split())
-        # для вершины vert_1 (vertexs[vert_1 - 1]) обновить список вершин
-        vertexs[vert_1].add(vert_2)
-        vertexs[vert_2].add(vert_1)
-
-    for count in range(num_vert + 1):
-        vertexs[count] = sorted(vertexs[count])
-    start_vertex = int(input())
+    
+    with open('input.txt') as file_in:
+        # кол-во вершин и рёбер
+        num_vert, num_edg = file_in.readline().split()
+        num_vert, num_edg = int(num_vert), int(num_edg)
+        vertexs = [list() for _ in range(num_vert + 1)]
+        # считывание и обработка рёбер
+        for _ in range(num_edg):
+            # вершины, соединяемые ребром
+            vert_1, vert_2 = file_in.readline().split()
+            vert_1, vert_2 = int(vert_1), int(vert_2)
+            
+            vertexs[vert_2].append(vert_1)
+            vertexs[vert_1].append(vert_2)
+            
+        for _ in range(len(vertexs)):
+            vertexs[_] = sorted(vertexs[_])
+        
+        
+        start_vertex = int(file_in.readline())
+        file_in.close()
 
     if num_vert == 1:
         print(start_vertex)
         return
 
-    main_bfs(vertexs, start_vertex)
+    result = main_bfs(vertexs, start_vertex)
+    print(*result)
 
 
 if __name__ == '__main__':
