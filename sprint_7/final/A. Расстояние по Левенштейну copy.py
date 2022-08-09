15ce4c80a7b0ebb9fb313fa1f630fcaf9c6fb6d0
coherentus <75427478+coherentus@users.noreply.github.com>
2022-08-09 08:40:51 +0300

    at home

--- sprint_7/final/A. Расстояние по Левенштейну copy.py
+++ sprint_7/final/A. Расстояние по Левенштейну copy.py
@@ -7,11 +7,26 @@ def get_levenshtein(string_one, string_two):
     if not (string_one and string_two):
         return max(len(string_one), len(string_two))
     
-    # для удобства работы алгоритма первой строкой берётся более длинная
+    # для удобства работы алгоритма первой строкой берётся более короткая
     if len(string_two) < len(string_one):
         string_one, string_two = string_two, string_one
     
-    
+    str_1, str_2 = string_one, string_two
+    n, m = len(string_one), len(string_two)
+    if n > m:
+        str_1, str_2 = string_two, string_one
+        n, m = m, n
+
+    current_row = range(n + 1)
+    for i in range(1, m + 1):
+        previous_row, current_row = current_row, [i] + [0] * n
+        for j in range(1, n + 1):
+            add, delete, change = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1]
+            if str_1[j - 1] != str_2[i - 1]:
+                change += 1
+            current_row[j] = min(add, delete, change)
+
+    return current_row[n]
 
 
 def main():
